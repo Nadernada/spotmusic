84add724881a147467f16a4c8d3e1a65fbfef0fe
Nadernada <59936807+Nadernada@users.noreply.github.com>
2023-09-25 01:19:11 +0100

    fixed deployment errors

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -2,7 +2,7 @@
 
 import AuthModal from "@/components/AuthModal"
 import Modal from "@/components/Modal"
-import UploadModal from "@/components/uploadModal"
+import UploadModal from "@/components/UploadModal"
 import { useEffect, useState } from "react"
 
 const ModalProvider = () => {
